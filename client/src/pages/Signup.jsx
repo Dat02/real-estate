fa8b0c368bbe4eeda4acf7d4d0e93ef43a4f506d
dat02 <datnguyendinh1311@gmail.com>
2024-09-12 13:23:37 +0700

    sign-in page func

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange  = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
@@ -27,7 +28,7 @@ export default function Signup() {
       });
       const data = await res.json();
       setLoading(false);
-      // setFormData({});
+      navigate('/sign-in')
 
       if(data.errors){
         setError(true);
